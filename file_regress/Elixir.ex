9f234826e4ecb0d5755b79e1166f3bffc1b275af
Nik <bybalesh234@gmail.com>
2025-02-03 12:06:52 +0600

    Update Elixir.ex

--- file_regress/Elixir.ex
+++ file_regress/Elixir.ex
@@ -1,7 +1,10 @@
 defmodule HelloWorld do
-  def print_hello_world(times) do
-    Enum.each(1..times, fn _ -> IO.puts("Hello World") end)
-  end
+
 end
 
 HelloWorld.print_hello_world(5)
+
+
+  def print_hello_world(times) do
+    Enum.each(1..times, fn _ -> IO.puts("Hello World") end)
+  end
