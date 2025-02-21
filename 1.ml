e70af8b7fb7849e1baf6c22783072c68e4cc6b59
Nik <bybalesh234@gmail.com>
2025-02-21 13:02:19 +0600

    Update 1.ml

--- 1.ml
+++ 1.ml
@@ -1,5 +1,3 @@
-// https://ocaml.org/manual/lex.html
-
 Prism.languages.ocaml = {
 	'comment': {
 		pattern: /\(\*[\s\S]*?\*\)/,
@@ -8,6 +6,12 @@ Prism.languages.ocaml = {
 let rec factorial n =
   if n <= 1 then 1
   else n * factorial (n - 1)
+
+(* Вызов функции и вывод результата *)
+let () =
+  let result = factorial 5 in
+  Printf.printf "Factorial of 5 is %d\n" result
+
 
 (* Вызов функции и вывод результата *)
 let () =
